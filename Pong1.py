c87a79b9388cb2156846c5cc0cd3ff83d09c6779
Shayla Sexton <sesexton@aggies.ncat.edu>
2020-01-16 14:18:13 -0500

    Creation of Paddle A

--- Pong1.py
+++ Pong1.py
@@ -8,7 +8,17 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350,0)
 
+# Paddle B
+
+# Ball
 
 # Main Game loop
 
