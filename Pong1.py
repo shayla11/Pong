716be5c304edd8167546eabc18c7659a4087c830
Shayla Sexton <sesexton@aggies.ncat.edu>
2020-01-15 23:35:53 -0500

    file clean up

---
+++ Pong1.py
@@ -0,0 +1,16 @@
+# Simple Pong for beginners
+
+import turtle
+
+window = turtle.Screen()
+window.title("Pong by Shayla Sexton")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+
+
+# Main Game loop
+
+while True:
+     window.update()
