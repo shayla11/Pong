11fd0bc1c306458782667f8550c0c6a45015fd31
Shayla Sexton <sesexton@aggies.ncat.edu>
2020-01-18 02:15:14 -0500

    half of paddle board collision

--- Pong1.py
+++ Pong1.py
@@ -91,3 +91,6 @@ while True:
      if ball.xcor() < -390:
          ball.goto(0,0)
          ball.dx *= -1
+     # Paddle + Ball Collision
+     if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+         ball.dx *= -1
